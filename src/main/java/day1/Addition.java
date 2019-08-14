e604f7932200fe4e368d77ed0ecee875eaf401e0
Keyne <you@example.com>
2019-08-14 00:45:05 -0400

    Added src and pom.xml

---
+++ src/main/java/day1/Addition.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class Addition {
+	public static int add(int a,int b)
+	{
+		int c=a+b;
+		return c;
+	}
+
+}
---
+++ src/test/java/day1/Addition.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class Addition {
+
+	public static void main(String[] args) {
+			int c= add(2,3);
+			System.out.println(c);
+			
+		}
+
+	}
+
+
